63d91a62b3bb542223336368f422312d677e9b9a
julian <julian@DESKTOP-JFUCUC8>
2018-12-13 08:22:28 +0100

    csv processor

--- src/io/CsvProcessor.java
+++ src/io/CsvProcessor.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -18,6 +20,8 @@ public class CsvProcessor {
     
     private static void readCsv(String filename, boolean hasHeader) {
         
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             
             String columnNames = hasHeader? br.readLine() : null;
@@ -25,7 +29,26 @@ public class CsvProcessor {
             String line;
             while ((line = br.readLine()) != null) {
                 
-                System.out.println(line);
+                String[] camps = line.split(",");
+                if (camps.length != 5) {
+                    continue;
+                }
+                
+                int edat = Integer.parseInt(camps[1]);
+                if (edat > max) {
+                    max = edat;
+                }
+                if (edat < min) {
+                    min = edat;
+                }
+                 
+                Map<String, Object> map = new HashMap<>();
+                map.put("nom", camps[0]);
+                map.put("edat", edat);
+                map.put("sou", Double.parseDouble(camps[2]));
+                map.put("ciutat", camps[3]);
+                map.put("estudis", Integer.parseInt(camps[4]));
+                System.out.println(map);
             }
             
         } catch (FileNotFoundException ex) {
@@ -33,5 +56,8 @@ public class CsvProcessor {
         } catch (IOException ex) {
             throw new RuntimeException("reading csv: " + filename, ex);
         }
+        
+        System.out.println("min: " + min);
+        System.out.println("max: " + max);
     }
 }
